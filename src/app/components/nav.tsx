3129b391e11d16ca983a219738c09bf6014094d3
DylBrad <dylanbradley1@hotmail.com>
2023-06-16 23:12:40 +0100

    Add react icons and styles to nav component

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,25 +1,37 @@
-import * as React from 'react';
-import Link from 'next/link';
+import * as React from "react";
+import Link from "next/link";
+import { GrMapLocation } from "react-icons/gr";
+import { FaUserAlt } from "react-icons/fa";
+import { CgFeed } from "react-icons/cg";
 
 const Nav = () => {
-    return (
-        <nav>
-        <div>
-          <h1>mAppy</h1>
-          <ul>
-            <li>
-              <Link href='/home'>Home</Link>
-            </li>
-            <li>
-              <Link href='/newsfeed'>Newsfeed</Link>
-            </li>
-            <li>
-              <Link href='/profile'>Profile</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    )
-}
+  return (
+    <nav className="main-navigation">
+      <div className="nav-top-section">
+        <h1>mAppy</h1>
+        <ul>
+          <li>
+            <GrMapLocation className="react-icons" size="24" />
+            <Link className="span" href="/home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <CgFeed className="react-icons" size="24" />
+            <Link className="span" href="/newsfeed">
+              Newsfeed
+            </Link>
+          </li>
+          <li>
+            <FaUserAlt className="react-icons" size="24" />
+            <Link className="span" href="/profile">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
