a0521dbf708f75c7feac426490f905b3f80b533e
DylBrad <dylanbradley1@hotmail.com>
2023-06-16 22:14:42 +0100

    Add home folder and nav component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,12 @@
-import styles from './page.module.css'
+import styles from '../page.module.css'
 
-import Nav from './components/nav'
+import Nav from '../components/nav'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Nav />
-      <h1>index</h1>
+      <h1>MAP</h1>
     </main>
   )
 }
