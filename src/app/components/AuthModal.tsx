61889e2c66d09694c64a910e473cfaa14d212402
DylBrad <dylanbradley1@hotmail.com>
2023-06-18 19:21:25 +0100

    Add api file

--- src/app/components/AuthModal.tsx
+++ src/app/components/AuthModal.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { useForm } from "react-hook-form";
 import { useCookies } from "react-cookie";
-// import { createUser, loginUser } from "../../API";
+import { createUser, loginUser } from "../API";
 
 const AuthModal = (props: any) => {
   let ErrorMessage: string;
